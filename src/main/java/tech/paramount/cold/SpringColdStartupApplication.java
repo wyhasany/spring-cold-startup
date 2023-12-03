38f86f530bd1d15263e6dd5bc4f4e8aca38d67f1
Michal Rowicki <michal.rowicki@vimn.com>
2023-12-03 22:28:16 +0100

    Init

---
+++ src/main/java/tech/paramount/cold/SpringColdStartupApplication.java
@@ -0,0 +1,12 @@
+package tech.paramount.cold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringColdStartupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringColdStartupApplication.class, args);
+	}
+}
---
+++ src/test/java/tech/paramount/cold/SpringColdStartupApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.paramount.cold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringColdStartupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
