b67e629dba300800b46d08aca915735b53e65514
Michal Rowicki <michal.rowicki@vimn.com>
2023-12-03 23:49:57 +0100

    Spring Native

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.2.0"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("org.graalvm.buildtools.native") version "0.9.28"
 }
 
 group = "tech.paramount"
@@ -22,4 +25,15 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+// Paketo buildpacks for aarch64
+// https://github.com/dashaun/paketo-arm64
+if (System.getProperty("os.arch") == "aarch64") {
+	tasks.named<BootBuildImage>("bootBuildImage") {
+		builder = "dashaun/builder:tiny"
+		environment = mapOf(
+				"BP_NATIVE_IMAGE" to "true"
+		)
+	}
 }
